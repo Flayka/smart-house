827af5aa8de457054109587f6173e188e07aca1a
Alexandra <rudakalexx@gmail.com>
2020-05-29 13:31:46 +0300

    Create program Condition

---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,28 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+
+    @Test
+    void shouldGetHigherTemp() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(34);
+        conditioner.increaseCurrentTemperature();
+        int actual = conditioner.getCurrentTemperature();
+        int expected = 35;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldGetLowerTemp() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(20);
+        conditioner.decreaseCurrentTemperature();;
+        int actual = conditioner.getCurrentTemperature();
+        int expected = 19;
+        assertEquals(expected, actual);
+    }
+}
