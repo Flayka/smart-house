4b75f64268f928a6b0a85b824b0b7bba9d6a04a6
Alexandra <rudakalexx@gmail.com>
2020-06-01 13:26:50 +0300

    Fix defects in Conditioner.java

--- src/main/java/ru/netology/domain/Conditioner.java
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -7,22 +7,6 @@ public class Conditioner {
     private int currentTemperature;
     private boolean on;
 
-//       public int getMaxTemperature() {
-//           return maxTemperature;
-//       }
-//
-//       public void setMaxTemperature(int maxTemperature) {
-//           this.maxTemperature = maxTemperature;
-//       }
-//
-//       public int getMinTemperature() {
-//           return minTemperature;
-//       }
-//
-//       public void setMinTemperature(int minTemperature) {
-//           this.minTemperature = minTemperature;
-//       }
-//
     public int getCurrentTemperature() {
         return currentTemperature;
     }
@@ -44,7 +28,6 @@ public class Conditioner {
             return;
         }
         currentTemperature++;
-        return;
     }
 
     public void decreaseCurrentTemperature() {
